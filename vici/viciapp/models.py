e360d4f8204a57e89fd85400087f652e12fe6674
Johan <johan.felisaz@prontmail.com>
2020-04-05 16:53:21 +0200

    added compression

--- vici/viciapp/models.py
+++ vici/viciapp/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 from tastypie.models import create_api_key
 
+import sys
+import PIL.Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
 # Generate an API key every time a User is created
 models.signals.post_save.connect(create_api_key, sender=User)
 
@@ -40,6 +45,17 @@ class Image(models.Model):
     # Fields
     legend = models.CharField(max_length=200)
     image = models.ImageField(upload_to=upload_image_dir_path)
+    
+    def save(self, *args, **kwargs):
+        instance = super(Image, self).save(*args, **kwargs)
+        print("{}".format(self.image.path))
+        image = PIL.Image.open(self.image.path)
+        print('Je compresse ...')
+        print("{}".format(self.image.path))
+        image.save(self.image.path, quality=20, optimize=True)
+        print("{}".format(self.image.path))
+        return instance
+            
 
     def __str__(self):
         return "Image : " + self.legend
