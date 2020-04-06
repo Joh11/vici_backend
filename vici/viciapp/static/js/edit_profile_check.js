a2563b82aef0c5fcd3d7dfa6ba0f48a8ab1d5055
Johan <johan.felisaz@prontmail.com>
2020-04-06 15:33:30 +0200

    commit stuff

---
+++ vici/viciapp/static/js/edit_profile_check.js
@@ -0,0 +1,12 @@
+
+function setupFormCheck() {
+  document.getElementById('changeProfileData').addEventListener('submit', (function(reader_array) {
+    return function(e) {
+      ///event is passed as 'e' in params
+      ///takes reader_array with file_reader_array as value
+      ///START SUBMIT CALLBACK//////////////////////////////////////////////////
+
+      ///END SUBMIT CALLBACK////////////////////////////////////////////////////
+    };
+  })(file_reader_array));
+}
